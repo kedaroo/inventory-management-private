058892382abefa17b90ce7669909d0c0dfef87ac
Kedar Basutkar <41827102+kedaroo@users.noreply.github.com>
2024-10-06 15:28:42 +0530

    remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { useInventoryStore } from "./stores/inventory-store";
 import InventoryStats from "./components/inventory-stats/inventory-stats";
 import InventoryTable from "./components/inventory-table/inventory-table";
 import Header from "./components/header/header";
-import "./App.css";
 
 function App() {
   const fetchInventory = useInventoryStore((state) => state.fetchInventory);
